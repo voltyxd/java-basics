98fc8a20fc02b8b5d086717af56f111d11210600
Andre Gustavo <andgouveiap@gmail.com>
2023-12-05 18:15:09 -0300

    tipos primitivos - convenções de variáveis

---
+++ src/academy/devdojo/maratonajava/introducao/TiposPrimitivos.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean
+        int idade = 10;
+
+        System.out.println("A idade é " + idade + " anos");
+
+    }
+}
