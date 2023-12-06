c6bdef8da86d36b590751befdeae4de851234e5a
Andre Gustavo <andgouveiap@gmail.com>
2023-12-06 11:41:30 -0300

    estruturas condicionais - if

---
+++ src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais.java
@@ -0,0 +1,24 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasCondicionais {
+    public static void main(String[] args) {
+        int idade = 15;
+        boolean isAutorizadoComprarBebida = idade > 18;
+        // !
+
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcóolica");
+        }
+
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcóolica");
+        }
+
+        boolean c = false;
+        if (c == true) {
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
+
+        System.out.println("Fora do if");
+    }
+}
