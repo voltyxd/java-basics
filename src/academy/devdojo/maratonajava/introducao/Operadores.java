c55c20ff35184efa6f251f0012be46a1dc739deb
Andre Gustavo <andgouveiap@gmail.com>
2023-12-05 21:23:06 -0300

    operadores - aritmeticos

---
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Operadores {
+    public static void main(String[] args) {
+        // + - / *
+
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = numero01 / numero02;
+
+        System.out.println(resultado);
+    }
+}
