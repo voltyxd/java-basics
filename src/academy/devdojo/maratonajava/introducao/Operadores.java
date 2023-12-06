c4e62478028d627b1d799dd50d5c48ce17f2eee3
Andre Gustavo <andgouveiap@gmail.com>
2023-12-05 22:50:04 -0300

    operadores - logicos AND

--- src/academy/devdojo/maratonajava/introducao/Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -14,15 +14,25 @@ public class Operadores {
         System.out.println(resto);
 
         // < > <= >= == !=
-        boolean isDezMaiorQueVinte = 10 > 20;
-        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezMaiorQueVinte = 10 >= 20;
+        boolean isDezMenorQueVinte = 10 <= 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezIgualDez = 10 == 10;
         boolean isDezDiferenteDez = 10 != 10;
+
         System.out.println("isDezMaiorQueVinte " + isDezMaiorQueVinte);
         System.out.println("isDezMenorQueVinte " + isDezMenorQueVinte);
         System.out.println("isDezIgualVinte " + isDezIgualVinte);
         System.out.println("isDezIgualDez " + isDezIgualDez);
         System.out.println("isDezDiferenteDez " + isDezDiferenteDez);
+
+        // && (AND) || (or) ! (no)
+        int idade = 29;
+        float salario = 3500F;
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta " + isDentroDaLeiMenorQueTrinta);
     }
 }
