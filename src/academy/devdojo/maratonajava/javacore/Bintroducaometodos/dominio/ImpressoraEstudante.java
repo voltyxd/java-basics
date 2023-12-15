7f3d6f4b413eee449257372d86ca5379707c7ca5
Andre Gustavo <andgouveiap@gmail.com>
2023-12-15 14:24:07 -0300

    orientação objetos - metodos pt 06 - parametros tipo referencia

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class ImpressoraEstudante {
+    public void imprime(Estudante estudante) {
+        System.out.println(estudante.nome);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante.idade);
+    }
+}
