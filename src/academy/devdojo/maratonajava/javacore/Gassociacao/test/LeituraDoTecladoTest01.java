fe6808a0c62e68c76bdd603773f5b1766ba10b23
Andre Gustavo <andgouveiap@gmail.com>
2023-12-20 16:45:51 -0300

    orientação objetos - leitura de dados pelo console

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/LeituraDoTecladoTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDoTecladoTest01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.print("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.print("Digite M ou F para seu sexo: ");
+        char sexo = sc.next().charAt(0);
+
+        System.out.println("----------------------------");
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+    }
+}
