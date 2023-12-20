6b23e6d30aeeb8cec20befa7fff8021c82ca30c9
Andre Gustavo <andgouveiap@gmail.com>
2023-12-20 13:48:01 -0300

    orientação objetos - bloco de inicializaçao estatico

---
+++ src/academy/devdojo/maratonajava/javacore/Fmodificadorestatico/dominio/Anime.java
@@ -0,0 +1,49 @@
+package academy.devdojo.maratonajava.javacore.Fmodificadorestatico.dominio;
+
+public class Anime {
+    private String nome;
+    private static int[] episodios;
+    // 0 - Bloco de inicialização é executado quando a JVM carregar a classe
+    // 1 - Alocado espaço em memória pro objeto
+    // 2 - Cada atributo de classe é criad oe inicializado com valores default ou o que for passada
+    // 3 - Bloco de inicialização é executado
+    // 4 - Construtor é executado
+    static {
+        System.out.println("Dentro do bloco de inicialização 1");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicialização estatico 2");
+    }
+
+    static {
+        System.out.println("Dentro do bloco de inicializaçã estaticoo 3");
+    }
+    {
+        System.out.println("Dentro do bloco de inicialização nao estatico");
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+
+        for (int episodio : episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
