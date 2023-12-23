03c8d322a57e794f128f6477f4ea90d98568fb17
Andre Gustavo <andgouveiap@gmail.com>
2023-12-23 14:53:06 -0300

    orientação objetos - interfaces pt 02 - implementando multiplas interfaces

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -8,7 +8,14 @@ public class DataLoaderTest01 {
     public static void main(String[] args) {
         DatabaseLoader databaseLoader = new DatabaseLoader();
         FileLoader fileLoader = new FileLoader();
+
         databaseLoader.load();
         fileLoader.load();
+
+        databaseLoader.remove();
+        fileLoader.remove();
+
+        databaseLoader.checkPermission();
+        fileLoader.checkPermission();
     }
 }
