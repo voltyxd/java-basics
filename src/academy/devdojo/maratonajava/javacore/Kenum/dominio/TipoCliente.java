dce103f29481e2b7aa2b673f518745170eda32e4
Andre Gustavo <andgouveiap@gmail.com>
2023-12-23 10:27:58 -0300

    orientação objetos - enumeraçao pt 04 - busca por atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/TipoCliente.java
@@ -1,8 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public enum TipoCliente {
-    PESSOA_FISICA(1, "Pessoa Fisica"),
-    PESSOA_JURIDICA(2, "Pessoa Juridica");
+    PESSOA_FISICA(1, "Pessoa Física"),
+    PESSOA_JURIDICA(2, "Pessoa Jurídica");
 
     private int valor;
     private String nomeRelatorio;
@@ -10,6 +10,15 @@ public enum TipoCliente {
     TipoCliente(int valor, String nomeRelatorio) {
         this.valor = valor;
         this.nomeRelatorio = nomeRelatorio;
+    }
+
+    public static TipoCliente tipoClientePorNomeRelatorio(String nomeRelatorio) {
+        for (TipoCliente tipoCliente : values()) {
+            if (tipoCliente.getNomeRelatorio().equals(nomeRelatorio)) {
+                return tipoCliente;
+            }
+        }
+        return null;
     }
 
     public int getValor() {
