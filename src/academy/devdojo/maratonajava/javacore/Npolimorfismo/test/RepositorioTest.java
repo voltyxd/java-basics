0927b165d89a57829bbf34c3ffef11e4711a2e24
Andre Gustavo <andgouveiap@gmail.com>
2023-12-25 11:21:16 -0300

    orientação objetos - polimorfismo pt 05 - programaçao orientada a interface

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTest.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioArquivo;
+
+public class RepositorioTest {
+    public static void main(String[] args) {
+        Repositorio repositorio = new RepositorioArquivo();
+        repositorio.salvar();
+    }
+}
