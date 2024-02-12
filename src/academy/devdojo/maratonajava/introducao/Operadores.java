3c69cd6e45b97b06f6b65bf07be870f1706209d7
Andre Gustavo <andgouveiap@gmail.com>
2024-02-12 14:56:50 -0300

    operadores pt 05 - atribuicao

--- src/academy/devdojo/maratonajava/introducao/Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Operadores.java
@@ -39,5 +39,24 @@ public class Operadores {
         float valorPlaystation = 5000;
         boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCincoCompravel " + isPlaystationCincoCompravel);
+
+        // = += -= *= /= %=
+        double bonus = 1800; // 1800
+        bonus += 1000; // 2800
+        bonus -= 1000; // 1800
+        bonus *= 2; // 3600
+        bonus /= 2; // 1800
+        bonus %= 2; // 0
+        System.out.println(bonus);
+
+        //
+        int contador = 0;
+        contador += 1; // contador = contador + 1
+        contador++;
+        contador--;
+        ++contador;
+        --contador;
+        int contador2 = 0;
+        System.out.println(++contador2);
     }
 }
