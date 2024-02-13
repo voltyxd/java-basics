e38e9b169b555644136d0f8297f6665f3ba2db78
Andre Gustavo <andgouveiap@gmail.com>
2024-02-13 11:53:25 -0300

    estruturas condicionais pt 07 - switch exercicio

---
+++ src/academy/devdojo/maratonajava/introducao/EstruturasCondicionais06Exercicio.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasCondicionais06Exercicio {
+    public static void main(String[] args) {
+        // Utilizando switch e dados os valores de 1 a 7, imprima se é dia útil ou final de semana
+        // Considerando 1 como Domingo
+        byte dia = 4;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia util");
+                break;
+            default:
+                System.out.println("Dia inválido.");
+        }
+    }
+}
