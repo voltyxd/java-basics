578ecf40f69718441b7109b509e51294b3223fdd
Andre Gustavo <andgouveiap@gmail.com>
2024-02-13 15:32:13 -0300

    estruturas de Repetição pt 05 - continue

--- src/academy/devdojo/maratonajava/introducao/EstruturasDeRepeticao05.java
+++ src/academy/devdojo/maratonajava/introducao/EstruturasDeRepeticao05.java
@@ -1,21 +1,16 @@
 package academy.devdojo.maratonajava.introducao;
 
-public class EstruturasDeRepeticao04 {
+public class EstruturasDeRepeticao05 {
     // Dado o valor de um carro, descubra em quantas ele pode ser parcelado
     // Condição valorParcela >= 1000;
     public static void main(String[] args) {
-        double valorCarro = 57000.00;
-        int parcela = 1;
-
-        while (true) {
+        double valorCarro = 30000;
+        for (int parcela = (int) valorCarro; parcela >= 1 ; parcela--) {
             double valorParcela = valorCarro / parcela;
-
             if (valorParcela < 1000) {
                 continue;
             }
-
-            System.out.printf("%dx de R$%.2f\n", parcela, valorParcela);
-            parcela++;
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
         }
     }
 }
