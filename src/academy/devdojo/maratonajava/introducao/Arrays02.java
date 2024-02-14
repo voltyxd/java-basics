611328b30b26b1f8c5feb054b68fb40286d79083
Andre Gustavo <andgouveiap@gmail.com>
2024-02-14 05:32:20 -0300

    arrays pt 02

---
+++ src/academy/devdojo/maratonajava/introducao/Arrays02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, float, double = 0
+        // char = '\u0000'
+        // boolean = false
+        // String = null
+        String[] nomes = new String[3];
+
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
