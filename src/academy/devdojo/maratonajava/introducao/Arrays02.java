acb408a08bcbf3fbd92243989ba7c69753140157
Andre Gustavo <andgouveiap@gmail.com>
2024-02-14 05:44:32 -0300

    arrays pt 03

--- src/academy/devdojo/maratonajava/introducao/Arrays02.java
+++ src/academy/devdojo/maratonajava/introducao/Arrays02.java
@@ -6,10 +6,14 @@ public class Arrays02 {
         // char = '\u0000'
         // boolean = false
         // String = null
-        String[] nomes = new String[3];
 
-        System.out.println(nomes[0]);
-        System.out.println(nomes[1]);
-        System.out.println(nomes[2]);
+        String[] nomes = new String[3];
+        nomes[0] = "Goku";
+        nomes[1] = "Kurosaki";
+        nomes[2] = "Luffy";
+
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
     }
 }
