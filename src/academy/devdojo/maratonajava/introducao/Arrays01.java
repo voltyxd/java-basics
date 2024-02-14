10e5639bdc2e57f8f11d78821639fa171ef0215a
Andre Gustavo <andgouveiap@gmail.com>
2024-02-14 05:27:27 -0300

    arrays pt 01

---
+++ src/academy/devdojo/maratonajava/introducao/Arrays01.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Arrays01 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 15;
+        idades[2] = 30;
+
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
