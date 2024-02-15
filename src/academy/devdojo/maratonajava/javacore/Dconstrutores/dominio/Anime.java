37d7f45f6551d48eafcca46a2ff8485441aacdce
Andre Gustavo <andgouveiap@gmail.com>
2024-02-15 13:53:26 -0300

    orientacao objetos - construtores pt 02 - sobrecarga

--- src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
+++ src/academy/devdojo/maratonajava/javacore/Dconstrutores/dominio/Anime.java
@@ -5,28 +5,23 @@ public class Anime {
     private String tipo;
     private int episodios;
     private String genero;
+    private String estudio;
 
     public Anime() {
-
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public Anime(String nome, String tipo, int episodios, String genero) {
-        System.out.println("Dentro do construtor");
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
     }
 
-    public void init(String nome, String tipo, int episodios) {
-        this.nome = nome;
-        this.tipo = tipo;
-        this.episodios = episodios;
-    }
-
-    public void init(String nome, String tipo, int episodios, String genero) {
-        this.init(nome, tipo, episodios);
-        this.genero = genero;
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
     public void imprime() {
@@ -34,6 +29,7 @@ public class Anime {
         System.out.println("Tipo: " + this.tipo);
         System.out.println("Episodios: " + this.episodios);
         System.out.println("Genero: " + this.genero);
+        System.out.println("Estudio: " + this.estudio);
     }
 
     public String getNome() {
