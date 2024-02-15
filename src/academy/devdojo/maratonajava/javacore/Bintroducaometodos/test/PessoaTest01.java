33c068a46a2f405b0a3173852430f16016bc13cf
Andre Gustavo <andgouveiap@gmail.com>
2024-02-15 09:32:22 -0300

    orientacao objetos - modificador de acesso private, get e set pt 02

--- src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -7,8 +7,8 @@ public class PessoaTest01 {
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("Jiraya");
         pessoa.setIdade(70);
-//        pessoa.imprime();
-        System.out.println(pessoa.getNome());
-        System.out.println(pessoa.getIdade());
+        pessoa.imprime();
+//        System.out.println(pessoa.getNome());
+//        System.out.println(pessoa.getIdade());
     }
 }
