938b1b5f4bf8dcb394d357eb264b49ab877b797d
Andre Gustavo <andgouveiap@gmail.com>
2024-02-17 18:39:10 -0300

    orientacao objetos - heranca pt 04 - construtores

--- src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -9,14 +9,12 @@ public class HerancaTest01 {
         Endereco endereco = new Endereco();
         endereco.setRua("Rua 3");
         endereco.setCep("012345-209");
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Toyohisa Shimazu");
+        Pessoa pessoa = new Pessoa("Toyohisa Shimazu");
         pessoa.setCpf("11111111");
         pessoa.setEndereco(endereco);
         pessoa.imprime();
 
-        Funcionario funcionario = new Funcionario();
-        funcionario.setNome("Oda Nobunaga");
+        Funcionario funcionario = new Funcionario("Oda Nobunaga");
         funcionario.setCpf("222222");
         funcionario.setEndereco(endereco);
         funcionario.setSalario(20000);
