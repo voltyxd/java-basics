f4f7e0e499eb7e4dab1e21cbbddea95324bce5b1
Andre Gustavo <andgouveiap@gmail.com>
2024-02-18 17:56:14 -0300

    orientacao objetos - enumeracao pt 04 - busca por atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -13,5 +13,9 @@ public class ClienteTest01 {
         System.out.println(cliente2);
         System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
         System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
+//        TipoCliente tipoCliente = TipoCliente.valueOf("Pessoa Física");
+//        System.out.println(tipoCliente);
+        TipoCliente tipoClient2 = TipoCliente.tipoClientePorNomeRelatorio("Pessoa Física");
+        System.out.println(tipoClient2);
     }
 }
