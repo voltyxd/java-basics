bfa36870bc884b3efb05b86078b7e0e52e3fb58f
Andre Gustavo <andgouveiap@gmail.com>
2024-02-18 17:03:01 -0300

    orientacao objetos - modificador final pt 03 - classes e metodos

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.test;
 
 import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class CarroTest01 {
     public static void main(String[] args) {
@@ -10,5 +11,9 @@ public class CarroTest01 {
         System.out.println(carro.COMPRADOR);
         carro.COMPRADOR.setNome("André");
         System.out.println(carro.COMPRADOR);
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("Enzo");
+        ferrari.imprime();
     }
 }
