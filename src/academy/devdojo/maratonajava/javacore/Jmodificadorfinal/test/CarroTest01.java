3b87f95e6f9f0a26764d899ef5d858ab1b3ec540
Andre Gustavo <andgouveiap@gmail.com>
2024-02-18 16:56:28 -0300

    orientacao objetos - modificador final pt 02 - tipo referencia

--- src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/test/CarroTest01.java
@@ -5,6 +5,10 @@ import academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("André");
+        System.out.println(carro.COMPRADOR);
     }
 }
