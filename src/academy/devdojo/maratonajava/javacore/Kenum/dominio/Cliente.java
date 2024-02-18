3bb4d1655f48c3b9d1d48599547cb3cb7232e05d
Andre Gustavo <andgouveiap@gmail.com>
2024-02-18 17:37:28 -0300

    orientacao objetos - enumeracao pt 02 - construtores e atributos

--- src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,19 +1,27 @@
 package academy.devdojo.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
+    public enum TipoPagamento {
+        DEBITO,
+        CREDITO
+    }
     private String nome;
     private TipoCliente tipoCliente;
+    private TipoPagamento tipoPagamento;
 
-    public Cliente(String nome, TipoCliente tipoCliente) {
+    public Cliente(String nome, TipoCliente tipoCliente, TipoPagamento tipoPagamento) {
         this.nome = nome;
         this.tipoCliente = tipoCliente;
+        this.tipoPagamento = tipoPagamento;
     }
 
     @Override
     public String toString() {
         return "Cliente{" +
                 "nome='" + nome + '\'' +
-                ", tipoCliente=" + tipoCliente +
+                ", tipoCliente=" + tipoCliente.getNomeRelatorio() +
+                ", tipoClienteInt=" + tipoCliente.getValor() +
+                ", tipoPagamento=" + tipoPagamento +
                 '}';
     }
 
@@ -31,5 +39,13 @@ public class Cliente {
 
     public void setTipoCliente(TipoCliente tipoCliente) {
         this.tipoCliente = tipoCliente;
+    }
+
+    public TipoPagamento getTipoPagamento() {
+        return tipoPagamento;
+    }
+
+    public void setTipoPagamento(TipoPagamento tipoPagamento) {
+        this.tipoPagamento = tipoPagamento;
     }
 }
