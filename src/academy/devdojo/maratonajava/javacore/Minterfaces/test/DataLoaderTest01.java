95f82a158655b69dac6772d2719f492fe7f1181d
Andre Gustavo <andgouveiap@gmail.com>
2024-02-19 11:21:09 -0300

    orientacao objetos - interfaces pt 03 - atributos e metodos estaticos

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -17,5 +17,8 @@ public class DataLoaderTest01 {
 
         databaseLoader.checkPermission();
         fileLoader.checkPermission();
+
+        DatabaseLoader.retrieveMaxDataSize();
+        DataLoader.retrieveMaxDataSize();
     }
 }
