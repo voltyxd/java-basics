0138c43365d6f41b8109082e81c20dc71494a502
Andre Gustavo <andgouveiap@gmail.com>
2024-02-20 10:32:14 -0300

    excecoes pt 05 - lancando excecao checked

--- src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -3,18 +3,19 @@ package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
 import java.io.File;
 import java.io.IOException;
 
-public class ExceptionTest01 {
-    public static void main(String[] args) {
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
         criarNovoArquivo();
     }
 
-    private static void criarNovoArquivo() {
+    public static void criarNovoArquivo() throws IOException {
         File file = new File("arquivo\\teste.txt");
         try {
             boolean isCriado = file.createNewFile();
             System.out.println("Arquivo criado: " + isCriado);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
+            throw e;
         }
     }
 }
