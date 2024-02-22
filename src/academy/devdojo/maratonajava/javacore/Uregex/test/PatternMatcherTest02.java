81aa2a9d3ded7196f5beb875649a6853501d423e
Andre Gustavo <andgouveiap@gmail.com>
2024-02-22 20:04:17 -0300

    classes utilitarias - regex pt 03 - pattern e matcher - range

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -3,11 +3,17 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "aba";
+        //  \d = Todos os dígitos
+        // \D = Tudo o que não for digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-ZA-Z, digitos, _
+        // \W = Tudo o que não for incluso no \w
+        String regex = "\\w";
 //        String texto = "abaaba";
-        String texto2 = "abababa";
+        String texto2 = "@#hh_j2 12gvh21";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("texto:  " + texto2);
@@ -15,7 +21,7 @@ public class PatternMatcherTest01 {
         System.out.println("regex " + regex);
         System.out.println("posicoes encontradas");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
         }
     }
 }
--- src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -3,11 +3,19 @@ package academy.devdojo.maratonajava.javacore.Uregex.test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest03 {
     public static void main(String[] args) {
-        String regex = "aba";
+        //  \d = Todos os dígitos
+        // \D = Tudo o que não for digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-ZA-Z, digitos, _
+        // \W = Tudo o que não for incluso no \w
+        // []
+//        String regex = "[a-zA-C]";
+        String regex = "0[xX][0-9a-f-A-F]";
 //        String texto = "abaaba";
-        String texto2 = "abababa";
+        String texto2 = "12 0x 0X 0xFFABC 0x109 0x1";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(texto2);
         System.out.println("texto:  " + texto2);
@@ -15,7 +23,7 @@ public class PatternMatcherTest01 {
         System.out.println("regex " + regex);
         System.out.println("posicoes encontradas");
         while (matcher.find()) {
-            System.out.print(matcher.start() + " ");
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
         }
     }
 }
