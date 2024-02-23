11a3430f296edb88fe78e78535ef5c53ef04231e
Andre Gustavo <andgouveiap@gmail.com>
2024-02-23 15:30:57 -0300

    classes utilitarias - io pt 04 - bufferedwriter

---
+++ src/academy/devdojo/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Vio.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try (FileWriter fw = new FileWriter(file, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+            bw.write("FileWriter test 123");
+            bw.newLine();
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
