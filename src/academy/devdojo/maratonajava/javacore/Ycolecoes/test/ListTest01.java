d291852bf9c327dd7cb7eafb45bf6125e9dfe8a7
Andre Gustavo <andgouveiap@gmail.com>
2024-02-25 15:06:49 -0300

    colecoes pt 07 - list pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -5,18 +5,28 @@ import java.util.List;
 
 public class ListTest01 {
     public static void main(String[] args) {
-        List<String> nomes = new ArrayList<>(); // 1.5
+        List<String> nomes = new ArrayList<>();
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("William");
-        nomes.add("DevDojo Academy");
+        nomes.add("DevDojo");
+        nomes2.add("Suane");
+        nomes2.add("Academy");
+
+//        System.out.println(nomes.remove("William"));
+        nomes.addAll(nomes2);
 
         for (String nome : nomes) {
             System.out.println(nome);
         }
-        nomes.add("Suane");
+
         System.out.println("--------------");
 
-        for (int i = 0; i < nomes.size(); i++) {
+        int size = nomes.size();
+        for (int i = 0; i < size; i++) {
             System.out.println(nomes.get(i));
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
     }
 }
