740634003b8d79b2dec401e8e3c906a1a0611e84
Andre Gustavo <andgouveiap@gmail.com>
2024-02-25 19:00:20 -0300

    colecoes pt 19 - map, hashmap. linkedhashmap pt 02

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,11 +1,12 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapTest01 {
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
+        Map<String, String> map = new LinkedHashMap<>();
         map.put("teklado", "teclado");
         map.put("mouze", "mouse");
         map.put("vc", "você");
