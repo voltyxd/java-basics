edf55036c6d963dbd1ce7eb5ee20fed64f52b0cf
Andre Gustavo <andgouveiap@gmail.com>
2024-02-26 15:10:25 -0300

    generics pt 04 - classes genericas pt 01

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/service/BarcoRentavelService.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.service;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarcoRentavelService {
+    private List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Canoa")));
+
+    public Barco buscarBarcoDisponivel() {
+        System.out.println("Buscando barco disponivel....");
+        Barco barco = barcosDisponiveis.remove(0);
+        System.out.println("Alugando barco: " + barco);
+        System.out.println("Barcos disponiveis para alugar:");
+        System.out.println(barcosDisponiveis);
+        return barco;
+    }
+
+    public void retornarBarcoAlugado(Barco barco) {
+        System.out.println("Devolvendo carro: " + barco);
+        barcosDisponiveis.add(barco);
+        System.out.println("Barcos disponiveis para alugar:");
+        System.out.println(barcosDisponiveis);
+
+    }
+
+}
