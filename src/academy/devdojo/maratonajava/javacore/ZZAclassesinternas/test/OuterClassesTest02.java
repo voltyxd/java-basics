0ebda9f1bfe8540381633616d3795eb68acc1b82
Andre Gustavo <andgouveiap@gmail.com>
2024-02-26 17:23:13 -0300

    classes internas pt 02 - classes locais

---
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest02 {
+    private String name = "Midoriya";
+
+    void print(final String param) {
+        final String lastName = "Izuku";
+        class LocalClass {
+            public void printLocal() {
+                System.out.println(param);
+                System.out.println(name + " " + lastName);
+            }
+        }
+        new LocalClass().printLocal();
+    }
+
+    public static void main(String[] args) {
+        OuterClassesTest02 outerClass = new OuterClassesTest02();
+        outerClass.print("parametro teste");
+    }
+}
