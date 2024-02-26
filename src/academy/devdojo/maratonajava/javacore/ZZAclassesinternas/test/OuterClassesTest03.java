3123d39e769c2273d40f76e6cf8c8964248fb49b
Andre Gustavo <andgouveiap@gmail.com>
2024-02-26 17:43:03 -0300

    classes internas pt 04 - classes aninhadas estaticas

---
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassesTest03.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassesTest03 {
+
+    private String name = "William";
+    static class Nested {
+        private String lastName = "Suane";
+        void print() {
+            System.out.println(new OuterClassesTest03().name + " " + lastName);
+        }
+    }
+
+    public static void main(String[] args) {
+        Nested nested = new Nested();
+        nested.print();
+    }
+}
