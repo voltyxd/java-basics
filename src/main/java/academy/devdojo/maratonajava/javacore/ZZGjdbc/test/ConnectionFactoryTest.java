118cb358ea3917fd8dc24bf20eeafb461d2433fc
Andre Gustavo <andgouveiap@gmail.com>
2024-03-06 17:18:21 -0300

    jdbc - adicionando dependencia e conectando com banco

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.ZZGjdbc.test;
+
+import academy.devdojo.maratonajava.javacore.ZZGjdbc.conn.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
