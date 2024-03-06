7645931e18e5428ea51a13951676e3f7943aad11
Andre Gustavo <andgouveiap@gmail.com>
2024-03-06 15:40:57 -0300

    jdbc - instalando e adicionando maven ao projeto

---
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/conn/ConnectionFactory.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratonajava.javacore.ZZGjdbc.conn;
+
+import java.sql.Connection;
+
+public class ConnectionFactory {
+    //java.sql = Connection, Statement, ResultSet, DriverManager
+
+}
