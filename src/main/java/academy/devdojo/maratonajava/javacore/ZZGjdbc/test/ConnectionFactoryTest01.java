20f6ab24f7a1275bc235ff1f627018010038c699
Andre Gustavo <andgouveiap@gmail.com>
2024-03-06 20:52:00 -0300

    jdbc - lombok e log4j2

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionFactoryTest01.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/test/ConnectionFactoryTest01.java
@@ -1,12 +1,13 @@
 package academy.devdojo.maratonajava.javacore.ZZGjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZGjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZGjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZGjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
