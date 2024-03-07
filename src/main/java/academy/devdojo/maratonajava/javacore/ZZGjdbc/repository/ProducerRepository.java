ca46bbe8ddb4f47fb769bcb00f73c5ee13e191aa
Andre Gustavo <andgouveiap@gmail.com>
2024-03-07 02:48:08 -0300

    jdbc - buscando dados com resultset pt 01 - findall

--- src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/repository/ProducerRepository.java
+++ src/main/java/academy/devdojo/maratonajava/javacore/ZZGjdbc/repository/ProducerRepository.java
@@ -5,8 +5,11 @@ import academy.devdojo.maratonajava.javacore.ZZGjdbc.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
@@ -15,7 +18,7 @@ public class ProducerRepository {
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-           log.info("Inserted producer '{}' in the database, rows affected '{}'", producer.getName(), rowsAffected);
+            log.info("Inserted producer '{}' in the database, rows affected '{}'", producer.getName(), rowsAffected);
         } catch (SQLException e) {
             log.error("Error while trying to insert producer '{}'", producer.getName(), e);
             throw new RuntimeException(e);
@@ -34,16 +37,37 @@ public class ProducerRepository {
         }
     }
 
-        public static void update(Producer producer) {
-            String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');"
-                    .formatted(producer.getName(), producer.getId());
-            try (Connection conn = ConnectionFactory.getConnection();
-                 Statement stmt = conn.createStatement()) {
-                int rowsAffected = stmt.executeUpdate(sql);
-                log.info("Updated producer '{}' in the database, rows affected '{}'", producer.getId(), rowsAffected);
-            } catch (SQLException e) {
-                log.error("Error while trying to update producer '{}'", producer.getId(), e);
-                throw new RuntimeException(e);
+    public static void update(Producer producer) {
+        String sql = "UPDATE `anime_store`.`producer` SET `name` = '%s' WHERE (`id` = '%d');"
+                .formatted(producer.getName(), producer.getId());
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement()) {
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Updated producer '{}' in the database, rows affected '{}'", producer.getId(), rowsAffected);
+        } catch (SQLException e) {
+            log.error("Error while trying to update producer '{}'", producer.getId(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all Produces");
+        String sql = "SELECT id, name FROM anime_store.producer;";
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+            while (rs.next()) {
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id"))
+                        .name(rs.getString("name"))
+                        .build();
+                producers.add(producer);
             }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producers", e);
+        }
+        return producers;
     }
 }
